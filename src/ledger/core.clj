47f7b4727cde3bd5dfb5bb2d2b8279a16b72d823
Poorna Shashank K <poorna.shashank@gmail.com>
2016-11-23 13:12:39 +0530

    Parsing multiple entries into Transaction entries
    - Assuming that all the lines in a file are split, trimmed and saved in
      a list, each of the line is parsed and associated with the date
      heading using loop .. recur form
    - Aliasing clojure.string ns to s to use with the methods in that ns
    - Added a data field :acct and :for to differentiate between bank,
      wallet, and self, friends accounts respectively
    - Cleaned up code for identifying the bal amounts using butlast method
      instead of doing a reverse of reverse on tokens
    - Using clj-time to parse the date headers in entries
    - Moving re for re-matches methods to vars

--- src/ledger/core.clj
+++ src/ledger/core.clj
@@ -1,4 +1,25 @@
-(ns ledger.core)
+(ns ledger.core
+  (require [clojure.string :as s]
+           [clj-time.core :as t]
+           [clj-time.format :as f]))
+
+(def current-year (t/year (t/now)))
+
+(def amt-re #"([-+*])?(\d+)")
+
+(def entry-re #"([-+*]?\d+)\s(.*)")
+
+(def date-re #"^(\d+)-(\w+)$")
+
+(defn date-entry? [line]
+  (re-matches date-re line))
+
+(defn text->date [text]
+  (str (f/unparse
+         (f/formatter "dd-MM")
+         (f/parse (f/formatter "dd-MMM") text))
+       "-"
+       current-year))
 
 (defn amt [a]
   (let [[_ t n] (re-matches #"([-+*])?(\d+)" a)]
@@ -6,13 +27,36 @@
      :txn ({"-" :debit "+" :credit "*" :update} t)}))
 
 (defn info [i]
-  (let [tokens (clojure.string/split i #" ")
-        [a b & the-rest] (reverse tokens)
+  (let [tokens (s/split i #" ")
+        [a the-rest] [(last tokens) (butlast tokens)]
+        [b the-rest-2] [(last the-rest) (butlast the-rest)]
         bal (if (= "bal" a) b (if (= "bal" b) a))
-        d (if (nil? bal) tokens (reverse the-rest))]
-    {:desc (clojure.string/join " " d)
+        d (if (nil? bal) tokens the-rest-2)]
+    {:desc (s/join " " d)
      :bal (if (not (nil? bal)) (Integer. bal))}))
 
 (defn text->entry [txt]
   (let [[_ a i] (re-matches #"([-+*]?\d+)\s(.*)" txt)]
-    (merge (amt a) (info i))))
+    (merge {:acct :wallet, :for :self}
+           (amt a)
+           (info i))))
+
+(defn text->full-entry [txt date i]
+  (merge {:id i}
+         {:day date}
+         (text->entry txt)))
+
+(defn lines->entries [lines]
+  (loop [date nil
+         s (filter #(not (empty? %)) lines)
+         entries []
+         id 0]
+    (if (not (seq s))
+      entries
+      (let [[date' s'] (if (date-entry? (first s))
+                         [(text->date (first s)) (rest s)]
+                         [date s])]
+          (recur date'
+                 (rest s')
+                 (conj entries (text->full-entry (first s') date' id))
+                 (inc id))))))
--- test/ledger/core_test.clj
+++ test/ledger/core_test.clj
@@ -2,7 +2,7 @@
   (:require [clojure.test :refer :all]
             [ledger.core :refer :all]))
 
-(deftest test-amt
+(deftest test-parse-amt
   (testing "testing valid debit amt fn"
     (let [a (amt "-256")]
       (is (= :debit (:txn a)))
@@ -16,7 +16,7 @@
       (is (= :update (:txn a)))
       (is (= 756 (:amt a))))))
 
-(deftest test-info
+(deftest test-parse-info
   (testing "testing info with bal fn"
     (let [i (info "groceries")]
       (is (= (:desc i) "groceries"))
@@ -37,3 +37,46 @@
       (is (= :debit (:txn entry)))
       (is (= "meds das" (:desc entry)))
       (is (= 540 (:bal entry))))))
+
+(deftest test-text->date
+  (let [year "2016"]
+    (testing  "dates parsing"
+      (is (= (text->date "02-jan") (str "02-01-" year)))
+      (is (= (text->date "02-january") (str "02-01-" year)))
+      (is (= (text->date "12-jan") (str "12-01-" year)))
+      (is (= (text->date "12-JAN") (str "12-01-" year)))
+      (is (= (text->date "29-Feb") (str "29-02-" year)))
+      (is (thrown? IllegalArgumentException (text->date "")))
+      (is (thrown? IllegalArgumentException (text->date "2-janry"))))))
+
+(deftest test-lines->entries
+  (let [lines ["24-jan"
+               "-133 exp1"
+               "-56 exp2"
+               ""
+               ""
+               "23-jan"
+               ""
+               "-67 exp3"
+               "+79 exp4"
+               "*678 wd #hdfc"
+               ""]
+        entries (lines->entries lines)]
+    (testing "empty lines are ignored"
+      ; The number of text entries have been converted only to trxn entries
+      (is (= (count entries) 5)))
+    (testing "each entry is tagged with a date and monotonically increasing id"
+        (is true (every? #(:day %) entries))
+        (is true (every? #(:id %) entries))
+        (is true (apply <= (map #(:id %) entries))))
+    (testing "testing that dates are assigned to entries correctly"
+        ; First 2 entries are of 1st day
+        (is true (every? #(= (:day %) "24-01-2016") (take 2 entries)))
+        ; Next 3 entries are of the 2nd day
+        (is true (every? #(= (:day %) "23-01-2016") (take-last 3 entries))))
+    (testing "amounts are assigned correctly to entries as per order"
+        (is (= 133 (:amt (get entries 0))))
+        (is (= 56 (:amt (get entries 1))))
+        (is (= 67 (:amt (get entries 2))))
+        (is (= 79 (:amt (get entries 3))))
+        (is (= 678 (:amt (get entries 4)))))))
